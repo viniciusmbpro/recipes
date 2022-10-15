fda36f23447709b15384eb39d344df90852301af
Vinícius Marques Bezerra <viniciusmbpro@gmail.com>
2022-10-15 08:11:13 -0300

    Finishing tests and adding coverage

--- recipes/tests/test_recipe_model_recipe.py
+++ recipes/tests/test_recipe_model_recipe.py
@@ -1,5 +1,7 @@
 from django.core.exceptions import ValidationError
-from django.forms import ValidationError
+from parameterized import parameterized
+
+from recipes.models import Recipe
 
 from .test_recipe_base import RecipeTestBase
 
@@ -9,8 +11,58 @@ class RecipeModelTest(RecipeTestBase):
         self.recipe = self.make_recipe()
         return super().setUp()
 
-    def test_recipe_title_raises_error_if_title_has_more_than_65_chars(self):
-        self.recipe.title = 'A' * 70
+    def make_recipe_no_defaults(self):
+        recipe = Recipe(
+            category=self.make_category(name='Category no default'),
+            author=self.make_author(username='Author no default'),
+            title='Title recipe no default',
+            description='Description recipe no default',
+            slug='recipe-slug',
+            preparation_time=10,
+            preparation_time_unit='Minutos',
+            servings=5,
+            servings_unit='Porções',
+            preparation_steps='Recipe Preparation Steps',
+            preparation_steps_is_html=False,
+            is_published=False,
+        )
+        recipe.full_clean()
+        recipe.save()
+        return recipe
 
+    @parameterized.expand([
+        ('title', 65),
+        ('description', 165),
+        ('preparation_time_unit', 65),
+        ('servings_unit', 65),
+    ])
+    def test_recipe_fields_max_length(self, field, max_length):
+        setattr(self.recipe, field, 'A' * (max_length + 1))
         with self.assertRaises(ValidationError):
             self.recipe.full_clean()
+
+    def test_recipe_preparation_steps_is_html_is_false_by_default(self):
+        recipe = self.make_recipe_no_defaults()
+        self.assertFalse(
+            recipe.preparation_steps_is_html,
+            msg='Recipe preparation_steps_is_html no is False by default',
+        )
+
+    def test_recipe_is_published_is_false_by_default(self):
+        recipe = self.make_recipe_no_defaults()
+        self.assertFalse(
+            recipe.is_published,
+            msg='Recipe is_published no is False by default',
+        )
+
+    def test_recipe_string_representation(self):
+        needed = 'Testing Representation'
+        self.recipe.title = needed
+        self.recipe.full_clean()
+        self.recipe.save()
+        self.assertEqual(
+            str(self.recipe),
+            needed,
+            msg=f'Recipe string representation must be "{needed}" '
+                f'but "{str(self.recipe)} was received"'
+        )
